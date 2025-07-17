840c63b59d4df45353c2be5d5c090229479cdda5
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-07-17 11:01:56 +0300

    cloudinary

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,43 @@
+require('dotenv').config({path: '../.env'});
+const cloudinary = require('cloudinary').v2;
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    
+    // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           '../images/pexels-pixabay-45201 (1).jpg', {
+               public_id: 'cat',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('profile', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('profile', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log(autoCropUrl);    
+})();

545f76f97c64e52ce5e22a96f536ef597ef1af84
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-07-18 23:10:29 +0300

    updated model project

--- backend/model/Project.js
+++ backend/model/Project.js
@@ -2,11 +2,11 @@ const { default: mongoose } = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    title: String,
+    title: {type: String, required: true},
     description: String,
     githubUrl: String,
     imageUrl: String,
-    techStack: [{String}]
+    techStack: [{type: String}]
 }, {timestamps: true});
 
 module.exports = mongoose.model('Project', ProjectSchema);

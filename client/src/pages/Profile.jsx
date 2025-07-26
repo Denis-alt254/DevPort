78a3bb75bddf440bb51443be956ba59a55a002bc
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-07-26 14:39:50 +0300

    profile page updated

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -38,27 +38,28 @@ function Profile(){
                 <img>{user?.profilePicUrl}</img>
             </div>
             <div className="bg-amber-50">
-                <div className="name">
-                    <p>Username: {user?.username}</p>
-                    <p>Role: {user?.role}</p>
+                <div className="flex justify-between m-3">
+                    <p>{user?.username}</p>
+                    <p>{user?.role}</p>
+                    <p>{user?.location}</p>
                 </div>
-                <div>Location: {user?.location}</div>
             </div>
+            <h2 className="text-center text-3xl text-amber-600">Bio</h2>
             <div className="bg-amber-50 mt-3">
-                Bio: {user?.bio}
+                {user?.bio}
             </div>
             <h2 className="text-center text-3xl text-amber-600 m-3">Skills</h2>
-            <div className="m-6 flex flex-col">
+            <div className="m-6 flex justify-between text-center bg-white p-5">
                 {user?.skills.map(skill => (
-                    <div className="" key={skill?._id}>
+                    <div className="bg-gray-400 rounded-full p-4" key={skill?._id}>
                         {skill?.name}
                     </div>
                 ))}
             </div>
-            <h2 className="text-center text-3xl text-amber-600">Projects</h2>
-            <div className="flex justify-between text-center ">
+            <h2 className="text-center text-3xl text-amber-600 mt-3">Projects</h2>
+            <div className="flex justify-between text-center bg-white m-6">
                 {projects?.map(project => (
-                    <div className="flex flex-col items-center m-6" key={project?._id}>
+                    <div className="flex flex-col items-center m-6 bg-[#6F8F63]" key={project?._id}>
                         <h2>{project?.title}</h2>
                         <div>
                             {/* <img>{project?.imageUrl}</img> */}
@@ -67,8 +68,8 @@ function Profile(){
                     </div>
                 ))}
             </div>
-            <div className="activity">
-                <h2>Activity</h2>
+            <div className="text-center mb-3">
+                <h2 className="text-3xl text-amber-600">Activity</h2>
                 <p>Recent Activity will be shown here.</p>
             </div>
         </div>

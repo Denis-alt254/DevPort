a90d1c0beea9d98523e7855e4a483cb0a1fa24d5
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-07-26 19:00:55 +0300

    changing to allow the added sidebar

--- client/src/layout/LayoutShell.jsx
+++ client/src/layout/LayoutShell.jsx
@@ -1,16 +1,24 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
+import SideBar from "../components/SideBar";
 
 export default function LayoutShell({children}){
     return(
+        <>
         <div className="layout">
-            <header>
-                <NavBar />
-            </header>
-            <main className="main">{children}</main>
-            <footer>
-                <Footer />
-            </footer>
+            <div className="sidebar">
+                <SideBar />
+            </div>
+            <div className="maincontent">
+                <header>
+                    <NavBar />
+                </header>
+                <main className="main">{children}</main>
+            </div>
         </div>
+        <footer>
+                <Footer />
+        </footer>
+        </>
     )
 }
